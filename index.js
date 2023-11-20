2b028d6794306c6b8283cab41a740b7f5589e250
DatsunCo <danya200375d@gmail.com>
2023-11-20 17:41:55 +0300

    reviwe

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const errorMiddleware = require('./middlewares/errorMiddleware')
 const POPT = process.env.PORT || 5000 
 const app = express()
 
+app.use(express.json())
 app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL
 }))
-app.use(express.json())
 app.use(cookieParser())
 app.use('/api', router)
 app.use(errorMiddleware)
