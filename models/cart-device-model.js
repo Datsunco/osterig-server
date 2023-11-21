47a138653cd868749880070cae3c559e6db23f4c
DatsunCo <danya200375d@gmail.com>
2023-11-21 14:15:13 +0300

    Bd models update

--- models/cart-device-model.js
+++ models/cart-device-model.js
@@ -3,9 +3,10 @@ const {Schema, model} = require('mongoose')
 const CartDeviceSchema = new Schema({
     CartDevice: {type: Schema.Types.ObjectId, ref: 'Cart'}, // Айдишник коризны к которому относиться товар
     productModel: {type: String, required: true},
-    typeId: {type: String, required: true},
+    catalogId: {type: String, required: true},
     count: {type: Number, required: true},
     productCode: {type: String, required: true},
+    brandNameEn: {type: String, required: true},
     productImageUrl: {type: String, required: true},
     price: {type: String, required: true},
     minCount: {type: Number, required: true}
--- models/favorites-device-model.js
+++ models/favorites-device-model.js
@@ -3,8 +3,9 @@ const {Schema, model} = require('mongoose')
 const FavoritesDeviceSchema = new Schema({
     favoritesDevice: {type: Schema.Types.ObjectId, ref: 'Favorites'}, // Айдишник избранного к которому относиться товар
     productModel: {type: String, required: true},
-    typeId: {type: String, required: true},
+    catalogId: {type: String, required: true},
     productCode: {type: String, required: true},
+    brandNameEn: {type: String, required: true},
     productImageUrl: {type: String, required: true},
     price: {type: String, required: true},
     minCount: {type: Number, required: true}
