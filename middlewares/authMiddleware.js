c9c6e067915ed4270967a7bfc93f1e6d74d0a388
DatsunCo <danya200375d@gmail.com>
2023-12-06 15:21:03 +0300

    testAuthMiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,18 +3,22 @@ const tokenService = require('../service/tokenService');
 
 module.exports = function (req, res, next) {
     try {
+        console.log(authorizationHeader)
         const authorizationHeader = req.headers.authorization;
         if (!authorizationHeader) {
+            console.log(1)
             return next(ApiError.UnauthorizedError());
         }
 
         const accessToken = authorizationHeader.split(' ')[1];
         if (!accessToken) {
+            console.log(2)
             return next(ApiError.UnauthorizedError());
         }
 
         const userData = tokenService.validateAccessToken(accessToken);
         if (!userData) {
+            console.log(3)
             return next(ApiError.UnauthorizedError());
         }
 
