f6fad9b1cf336719793a058382f74ea89373bab8
DatsunCo <danya200375d@gmail.com>
2024-01-19 16:07:33 +0300

    fix: refresh test

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -29,7 +29,8 @@ class UserController {
             res.cookie('refreshToken', userData.refreshToken, {
                 maxAge: 14 * 24 * 60 * 60 * 1000,
                 httpOnly: true,
-                sameSite: 'none'
+                sameSite: 'none',
+                secure: true
               })
             return res.json(userData);
         } catch (e) {
