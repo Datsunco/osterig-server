165ff77e31c1df715d6c29528df98b45f4f0415f
DatsunCo <danya200375d@gmail.com>
2024-05-19 22:16:42 +0300

    check

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -2,9 +2,10 @@ const ApiError = require('../exceptions/apiError');
 
 module.exports = function (err, req, res, next) {
     console.log(err);
+    console.log('check')
     if (err instanceof ApiError) {
         return res.status(err.status).json({message: err.message, errors: err.errors})
     }
-    return res.status(500).json({message: 'Непредвиденная ошибка'})
+    return res.status(500).json({message: 'Непредвиденная ошибка', err})
 
-};
+}; 
