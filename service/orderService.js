415bf22a84df1d7f20b8e60120688220a0373250
DatsunCo <danya200375d@gmail.com>
2024-07-07 16:31:36 +0300

    dev: payment

--- service/orderService.js
+++ service/orderService.js
@@ -4,14 +4,14 @@ const cartDeviceModel = require('../models/cart-device-model')
 const ApiError = require('../exceptions/apiError')
 const { v4: uuidv4 } = require('uuid');
 const axios = require('axios')
-const checkout =  require('./yoouKassaConfig')
+const checkout = require('./yoouKassaConfig')
 
 
-class OrderService{
-    async createOrder(userId, device, paymentType){
-        
+class OrderService {
+    async createOrder(userId, device, paymentType) {
 
-        
+
+
         const resp = await axios.get(`https://www.cbr-xml-daily.ru/daily_json.js`)
         const usd = resp.data.Valute['USD'].Value
         // console.log("cart", device)
@@ -26,14 +26,39 @@ class OrderService{
             payment_method_data: {
                 type: 'bank_card'
             },
+            receipt: {
+                customer: {
+                    email: "danya200375@gmail.com"
+                },
+                items: [
+                    {
+                        description: "Топ трикотажный",
+                        quantity: "1.00",
+                        amount: {
+                            value: "10.00",
+                            currency: "RUB"
+                        },
+                        vat_code: "4",
+                        payment_mode: "full_prepayment",
+                        payment_subject: "marked",
+                        mark_mode: 0,
+                        // mark_code_info:
+                        // {
+                        //     "gs_1m": "DFGwNDY0MDE1Mzg2NDQ5MjIxNW9vY2tOelDFuUFwJh05MUVFMDYdOTJXK2ZaMy9uTjMvcVdHYzBjSVR3NFNOMWg1U2ZLV0dRMWhHL0UrZi8ydkDvPQ=="
+                        // },
+                        measure: "piece"
+                    }
+                ]
+            },
             confirmation: {
                 type: 'redirect',
                 return_url: 'https://osterrig-electronics.ru/return-url' // Укажите URL для возврата после оплаты
             }
         };
-        
+        checkout.cre
+
         try {
-            const cartData = await orderModel.create({order: userId, deviceList: device})
+            const orderData = await orderModel.create({ order: userId, deviceList: device })
             const payment = await checkout.createPayment(createPayload, idempotenceKey);
             console.log(payment);
 
@@ -51,8 +76,8 @@ class OrderService{
         }
     }
 
-    async getOrders(userId){
-        const ordersData = await orderModel.find({order: userId})
+    async getOrders(userId) {
+        const ordersData = await orderModel.find({ order: userId })
 
         return ordersData
     }
