89627f77dcdd77137764a065f60c22159dd269c9
DatsunCo <danya200375d@gmail.com>
2024-07-07 14:45:27 +0300

    dev: payment

--- service/orderService.js
+++ service/orderService.js
@@ -12,7 +12,7 @@ class OrderService{
         
 
         
-        const usd = await axios.get(`https://www.cbr-xml-daily.ru/daily_json.js`).data.Valute['USD']
+        const usd = await axios.get(`https://www.cbr-xml-daily.ru/daily_json.js`).data.Valute['USD'].Value
 
         // console.log("cart", device)
         console.log('usd', usd)
